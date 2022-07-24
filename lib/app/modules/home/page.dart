de9ba18d5429b747f6cf8fe90c5524400b360696
Gabriel Wisky Benatti <gabrielwiskybenatti@gmail.com>
2022-07-24 13:37:49 -0300

    melhorias na listagem dos estabelecimentos
    aula 64

--- lib/app/modules/home/page.dart
+++ lib/app/modules/home/page.dart
@@ -1,5 +1,6 @@
 import 'package:app_hortifruti_pratico/app/modules/home/controller.dart';
 import 'package:flutter/material.dart';
+import 'package:transparent_image/transparent_image.dart';
 import 'package:get/get.dart';
 
 class HomePage extends GetView<HomeController> {
@@ -14,9 +15,39 @@ class HomePage extends GetView<HomeController> {
           children: [
             for (var estabelecimento in state!)
               ListTile(
-                leading: const FlutterLogo(),
+                onTap: () {},
+                contentPadding:
+                    const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                leading: Container(
+                  width: 56,
+                  height: 56,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(8.0),
+                    child: FadeInImage.memoryNetwork(
+                      placeholder: kTransparentImage,
+                      image: estabelecimento.image,
+                    ),
+                  ),
+                ),
                 title: Text(estabelecimento.nome),
-                trailing: Text(estabelecimento.isOnline ? 'Aberto' : 'Fechado'),
+                trailing: Container(
+                  padding: const EdgeInsets.all(4.0),
+                  decoration: BoxDecoration(
+                    color: estabelecimento.isOnline
+                        ? Colors.green
+                        : Colors.black45,
+                    border: Border.all(
+                      color: Colors.black12,
+                      width: 2.0,
+                    ),
+                  ),
+                  child: Text(
+                    estabelecimento.isOnline ? 'Aberto' : 'Fechado',
+                    style: Get.textTheme.bodyMedium!.copyWith(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
               )
           ],
         ),
@@ -32,6 +63,7 @@ class HomePage extends GetView<HomeController> {
         //     ),
         //   ),
         // ),
+
         onEmpty: const Padding(
           padding: EdgeInsets.all(16.0),
           child: Center(
