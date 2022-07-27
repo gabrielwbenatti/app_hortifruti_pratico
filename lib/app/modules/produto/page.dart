5ee59739a54b01b4b15fd1949514b87747f8b37a
Gabriel Wisky Benatti <gabrielwiskybenatti@gmail.com>
2022-07-26 21:35:55 -0300

    add imagem do produto
    aula 71

--- lib/app/modules/produto/page.dart
+++ lib/app/modules/produto/page.dart
@@ -1,4 +1,5 @@
 import 'package:app_hortifruti_pratico/app/modules/produto/controller.dart';
+import 'package:transparent_image/transparent_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -7,14 +8,38 @@ class ProdutoPage extends GetView<ProdutoController> {
 
   @override
   Widget build(BuildContext context) {
+    var produto = controller.produto.value;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          controller.produto.value!.nome,
+          produto!.nome,
         ),
       ),
-      body: const SafeArea(
-        child: Text('ProdutoController'),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(
+            vertical: 8.0,
+            horizontal: 16.0,
+          ),
+          child: Column(
+            children: [
+              if (produto.imagem!.isNotEmpty)
+                Align(
+                  child: Container(
+                    padding: const EdgeInsets.only(bottom: 8.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(16.0),
+                      child: FadeInImage.memoryNetwork(
+                        placeholder: kTransparentImage,
+                        image: produto.imagem!,
+                      ),
+                    ),
+                  ),
+                ),
+            ],
+          ),
+        ),
       ),
     );
   }
